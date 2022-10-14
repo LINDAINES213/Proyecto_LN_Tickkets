e9887ce936d39f5ba846466197492bb505e4a6dc
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2022-10-14 00:44:15 -0600

    Splash Screen

--- app/src/main/java/com/example/ln_tickets/MainActivity.kt
+++ app/src/main/java/com/example/ln_tickets/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.ln_tickets
 
+import android.content.Intent
 import   androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
+    val DURACION: Long = 3000;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val logo = findViewById<ImageView>(R.id.imageView2)
+
+
+
+        Glide.with(this).load(R.drawable.logo_1).into(logo)
+
+        cambiarActivity()
+    }
+
+    private fun cambiarActivity(){
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }, DURACION)
     }
 }
