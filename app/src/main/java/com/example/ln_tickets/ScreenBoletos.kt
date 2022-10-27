e84959fe13c0e86602aa08c600fb07fe622690db
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2022-10-27 09:19:24 -0600

    ScreenBoletos

--- app/src/main/java/com/example/ln_tickets/ScreenBoletos.kt
+++ app/src/main/java/com/example/ln_tickets/ScreenBoletos.kt
@@ -3,11 +3,9 @@ package com.example.ln_tickets
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class HomePage : AppCompatActivity() {
+class ScreenBoletos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
-
-        
+        setContentView(R.layout.screen_boletos)
     }
 }
