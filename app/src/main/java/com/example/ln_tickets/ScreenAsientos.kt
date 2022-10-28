9cb686ba4dfce5b36fcc4bd6e7f02b77dd09c575
Mario Guerra <gue21008@uvg.edu.gt>
2022-10-27 19:37:14 -0600

    Creación ScreenAsientos, ScreenBoletoVendido y actualización ScreenBoletos

---
+++ app/src/main/java/com/example/ln_tickets/ScreenAsientos.kt
@@ -0,0 +1,31 @@
+package com.example.ln_tickets
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ScreenAsientos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_asientos)
+
+        val boletos = findViewById<Button>(R.id.boletos)
+        boletos.setOnClickListener{
+            val pantallaboletos = Intent(this, ScreenBoletos::class.java)
+            startActivity(pantallaboletos)
+        }
+
+        val inicio = findViewById<Button>(R.id.inicio)
+        inicio.setOnClickListener{
+            val pantallainicio = Intent(this, HomePage::class.java)
+            startActivity(pantallainicio)
+        }
+
+        val noticias = findViewById<Button>(R.id.noticias)
+        noticias.setOnClickListener{
+            val pantallanoticias = Intent(this, ScreenNoticias::class.java)
+            startActivity(pantallanoticias)
+        }
+    }
+}
