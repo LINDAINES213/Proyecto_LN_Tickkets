f09130ff53070810fa72d5644814ad7fe854b605
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2022-11-25 05:47:51 -0600

    datos de compra

---
+++ app/src/main/java/com/example/ln_tickets/CompraFinal.kt
@@ -0,0 +1,24 @@
+package com.example.ln_tickets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class CompraFinal : AppCompatActivity() {
+
+    private var nombre2:TextView?=null
+   // private var asientos2:TextView?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compra_final)
+
+        nombre2=findViewById(R.id.nombre2)
+       //asientos2=findViewById(R.id.asientos2)
+        val nom2=intent.getStringExtra("nom2")
+        val asi2 = intent.getStringExtra("asi2")
+        nombre2?.text="Nombre Comprador: $nom2"
+       // asientos2?.text="Cantidad de Asientos: $asi2"
+
+    }
+}
--- app/src/main/java/com/example/ln_tickets/ScreenDatosCompra.kt
+++ app/src/main/java/com/example/ln_tickets/ScreenDatosCompra.kt
@@ -3,13 +3,21 @@ package com.example.ln_tickets
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 
 class ScreenDatosCompra : AppCompatActivity() {
+
+    private var nombre:EditText?=null
+    private var asientos:EditText?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screen_datos_compra)
+
+        nombre=findViewById(R.id.nombre)
 
         val boletos = findViewById<Button>(R.id.boletos)
         boletos.setOnClickListener{
@@ -28,5 +36,16 @@ class ScreenDatosCompra : AppCompatActivity() {
             val pantallanoticias = Intent(this, ScreenNoticias::class.java)
             startActivity(pantallanoticias)
         }
+
+
+    }
+
+    fun Enviar(view: View){
+        var nom=nombre?.text.toString()
+        //var asi=asientos?.text.toString()
+        var intento=Intent(this, CompraFinal::class.java)
+        intento.putExtra("nom", nom)
+        //intento.putExtra("asi", asi)
+        startActivity(intento)
     }
 }
