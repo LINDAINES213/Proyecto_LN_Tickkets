f806f233ef14a56419045869989144e637f6020a
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2022-11-25 07:47:42 -0600

    codigo QR

--- app/src/main/java/com/example/ln_tickets/ScreenDatosCompra.kt
+++ app/src/main/java/com/example/ln_tickets/ScreenDatosCompra.kt
@@ -1,23 +1,28 @@
 package com.example.ln_tickets
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 
 class ScreenDatosCompra : AppCompatActivity() {
-
-    private var nombre:EditText?=null
-    private var asientos:EditText?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screen_datos_compra)
 
-        nombre=findViewById(R.id.nombre)
+        var dpi: EditText=findViewById(R.id.dpi)
+        var codigo: ImageView=findViewById(R.id.codigo)
+
+        var button: Button = findViewById(R.id.button)
+
 
         val boletos = findViewById<Button>(R.id.boletos)
         boletos.setOnClickListener{
@@ -37,15 +42,33 @@ class ScreenDatosCompra : AppCompatActivity() {
             startActivity(pantallanoticias)
         }
 
+        button.setOnClickListener(View.OnClickListener {
+            try{
+                var barcodeEncoder: BarcodeEncoder = BarcodeEncoder()
+                var bitmap: Bitmap = barcodeEncoder.encodeBitmap(
+                    dpi.text.toString(),
+                    BarcodeFormat.QR_CODE,
+                    750,
+                    750,
+                )
+
+                codigo.setImageBitmap(bitmap)
+
+            } catch (e: java.lang.Exception){
+                e.printStackTrace()
+            }
+        })
+
 
     }
 
-    fun Enviar(view: View){
+   /** fun Enviar(view: View){
         var nom=nombre?.text.toString()
-        //var asi=asientos?.text.toString()
+        var asi=asientos?.text.toString()
         var intento=Intent(this, CompraFinal::class.java)
         intento.putExtra("nom", nom)
-        //intento.putExtra("asi", asi)
+        intento.putExtra("asi", asi)
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder()
         startActivity(intento)
-    }
+    }*/
 }
